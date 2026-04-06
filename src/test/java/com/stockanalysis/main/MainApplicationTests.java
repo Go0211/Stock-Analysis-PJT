7f373ac04ad756c923f3db3ffc6b5ea6963023a3
Ruth <v12rhtjdxo@naver.com>
2026-04-06 17:00:23 +0900

    Initial commit

---
+++ src/test/java/com/stockanalysis/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockanalysis.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
